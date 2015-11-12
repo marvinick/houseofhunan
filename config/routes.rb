079bd0b922783fa06ef0decd8a008e5fe59bfe23
marvin <marvkipi@gmail.com>
2015-11-12 15:31:41 -0800

    redirect to root path when no routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :skip => :registrations
+
   root 'pages#home'
 
   resources :menus do
   	resources :foods 
   end
+
+  #redirect no routes exception
+  get '*path' => redirect('/')
+
 end
