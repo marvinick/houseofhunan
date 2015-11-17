342a36581fae6dffae2a2f58ba297d2be9736e5b
marvin <marvkipi@gmail.com>
2015-11-16 21:37:17 -0800

    no s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     :provider              => "AWS",
     :aws_access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
     :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
-    :region                => 'us-west-1'
+    :region                => 'us-west-1',
+    :path_style            => true
   }
   config.fog_directory = ENV["AWS_BUCKET"]
   config.fog_public     = false                                  # optional, defaults to true
